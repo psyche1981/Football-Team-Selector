5cee07b3bf39796c3dc9001067dee42c79af1c94
Simon Jones <spjones1981@gmail.com>
2015-08-30 21:10:07 +0100

    Load in the files an increment the numElements

--- TeamSelector/TeamSelector/main.cpp
+++ TeamSelector/TeamSelector/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 class InfoLoader
 {
@@ -9,22 +10,33 @@ public:
 		numTeams = 0;
 		numPlayers = 0;
 
-		std::ifstream teams(TeamsFileName);
+		std::ifstream teams;
+		teams.open(TeamsFileName);
 		if (teams.fail())
 		{
 			std::cerr << "File: " << TeamsFileName.c_str() << " does not exist" << std::endl;
-			std::exit(-1);
+			system("PAUSE");
 		}
 
-		std::ifstream players(PlayersFileName);
+		std::ifstream players;
+		players.open(PlayersFileName);
 		if (players.fail())
 		{
 			std::cerr << "File: " << PlayersFileName.c_str() << " does not exist" << std::endl;
-			std::exit(-1);
+			system("PAUSE");
 		}
 
-
 		//TODO read in the data from each file
+		std::string line;
+		while (std::getline(teams, line))
+		{
+			numTeams++;
+		}
+
+		while (std::getline(players, line))
+		{
+			numPlayers++;
+		}
 		//TODO choose the best container for the teams and the players
 
 	}
@@ -37,8 +49,9 @@ public:
 
 int main(int argc, char** argv)
 {
-	std::cout << "Working" << std::endl;
-	std::cout << "Please enter the file name (including extension) of the file containing the list of teams" << std::endl;
+	std::cout << "Please make sure there are two .txt files int he same directory as the executable. "<<
+		std::endl << "One containing Teams and the other containing the names of the people in the round." << std::endl <<
+	"Enter the file name (including extension) of the file containing the list of teams" << std::endl;
 
 	char* s = new char[100];
 	std::cin >> s;
@@ -53,5 +66,6 @@ int main(int argc, char** argv)
 	std::cout << "Number of Teams = " << info.numTeams <<
 		", Number of Players = " << info.numPlayers << std::endl;
 
+	std::cin >> s;
 	return 0;
 }
