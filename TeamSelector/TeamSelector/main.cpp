c0a87cdc545bf0497f9b04a996a1bf70f5d3d51b
Simon Jones <spjones1981@gmail.com>
2015-08-31 19:26:54 +0100

    finished the main logic

--- TeamSelector/TeamSelector/main.cpp
+++ TeamSelector/TeamSelector/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <unordered_map>
+#include <map>
+#include <random>
+#include <ctime>
 
 class InfoLoader
 {
 public:
-	InfoLoader(const std::string& TeamsFileName, const std::string& PlayersFileName)
+	InfoLoader()
+	{}
+
+	int init(const std::string& TeamsFileName, const std::string& PlayersFileName)
 	{
 		numTeams = 0;
 		numPlayers = 0;
@@ -16,7 +21,7 @@ public:
 		if (teams.fail())
 		{
 			std::cerr << "File: " << TeamsFileName.c_str() << " does not exist" << std::endl;
-			system("PAUSE");
+			return -1;
 		}
 
 		std::ifstream players;
@@ -24,14 +29,14 @@ public:
 		if (players.fail())
 		{
 			std::cerr << "File: " << PlayersFileName.c_str() << " does not exist" << std::endl;
-			system("PAUSE");
+			return -1;
 		}
 
-		//TODO read in the data from each file
 		std::string line;
 		while (std::getline(teams, line))
 		{
 			numTeams++;
+			teamsMap[numTeams] = line;
 		}
 
 		while (std::getline(players, line))
@@ -42,43 +47,48 @@ public:
 
 		teams.close();
 		players.close();
-		//TODO choose the best container for the teams and the players
-
+		return 0;
 	}
 
 	void saveFile()
 	{
-		//Placeholder code until output is done		
 		std::ofstream data;
 		data.open("Results.txt");
 		std::string s;
 		for (auto& it = results.begin(); it != results.end(); it++)
-		{
-			s = it->first + it->second + "\n";
+		{			
+			s = it->first + ": " + it->second + "\n";
 			data.write(s.c_str(), s.size());
 		}
 		data.close();
 	}
-
+	
 private:
 	std::string generateTeam()
 	{
-		std::string team = ": Fish";
-		//do random thing here and return the team
+		std::string team = "";
+		static std::mt19937 randomEngine((unsigned int)time(nullptr));
+		static std::uniform_int_distribution<int> roll(1, numTeams);
+		int teamPick = roll(randomEngine);
+		numTeams--;
+		team = teamsMap[teamPick];
+		teamsMap[teamPick].erase();
 
 		return team;
 	}
 
 	int numTeams;
 	int numPlayers;
-	std::unordered_map<std::string, std::string> results;
+	std::map<std::string, std::string> results;
+	std::map<int, std::string> teamsMap;
 };
+	
 
 
 int main(int argc, char** argv)
 {
-	std::cout << "Please make sure there are two .txt files int he same directory as the executable. "<<
-		std::endl << "One containing Teams and the other containing the names of the people in the round." << std::endl <<
+	std::cout << "Please make sure there are two .txt files int he same directory as the executable, "<<
+		std::endl << "one containing Teams and the other containing the names of the people in the round." << std::endl <<
 	"Enter the file name (including extension) of the file containing the list of teams" << std::endl;
 
 	char* s = new char[100];
@@ -90,9 +100,11 @@ int main(int argc, char** argv)
 	std::cin >> s;
 	std::string playersFileName = s;
 
-	InfoLoader info(teamFileName, playersFileName);
-	info.saveFile();
+	InfoLoader info;
+	if(info.init(teamFileName, playersFileName) == 0)
+		info.saveFile();
 
+	system("PAUSE");
 	delete[] s;
 	return 0;
 }
